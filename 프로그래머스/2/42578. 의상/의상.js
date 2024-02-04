519d4f724308cebc2ecac9a4a2b94c7e46e23d9c
SuBinHwang <103632489+subin114@users.noreply.github.com>
2024-02-04 19:30:26 +0900

    [level 2] Title: 의상, Time: 4.96 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,10 +1,11 @@
 function solution(clothes) {
-  let map = [];
+  let map = {};
   let result = 1;
 
   for (let i = 0; i < clothes.length; i++) {
     if (map[clothes[i][1]] >= 1) map[clothes[i][1]]++;
     else map[clothes[i][1]] = 1;
+    console.log(map)
   }
 
   for (let m in map) result *= map[m] + 1;
