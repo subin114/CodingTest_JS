ace6aa19bcca728c336c915a7f5358e0a2e32b3f
SuBinHwang <103632489+subin114@users.noreply.github.com>
2024-02-04 23:15:30 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -2,9 +2,7 @@ function solution(array, height) {
     var answer = 0;
     
     for (let i = 0; i < array.length; i++) {
-        if (array[i] > height) {
-            answer++;
-        }
+        if (array[i] > height) answer++;
     }
         
     return answer;
