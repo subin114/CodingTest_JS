1d4961e68797efb862786f7d5fc14183f45a9add
SuBin Hwang <103632489+subin114@users.noreply.github.com>
2024-02-18 16:57:54 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 28.28 ms, Memory: 90.7 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,7 +1,7 @@
 function solution(arr) {
     let stack = [];
     for (let i of arr) {
-        if (stack.length === 0 || stack[stack.length - 1] !== i) {
+        if (stack[stack.length - 1] !== i) {
             stack.push(i)
         }
     }
