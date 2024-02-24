1002b5c05de4f87906e9cba9ecefee998987f1f2
SuBin Hwang <103632489+subin114@users.noreply.github.com>
2024-02-24 15:45:51 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.js
@@ -0,0 +1,11 @@
+function solution(num_list) {
+    let odd = '';
+    let even = '';
+    
+    for (let num of num_list) {
+        if (num % 2 === 0) even += num;
+        else odd += num;
+    }
+    
+    return parseInt(odd) + parseInt(even)
+}
