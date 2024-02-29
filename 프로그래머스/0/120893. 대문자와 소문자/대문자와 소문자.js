4d62463743aef5a558b2f871bcddbfefb40435bb
SuBin Hwang <103632489+subin114@users.noreply.github.com>
2024-02-29 23:55:52 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.29 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,11 +1,9 @@
 function solution(my_string) {
-    let arr = [...my_string];
-    
-    let res = arr.map((i) => {
+    my_string = my_string.split('').map((i) => {
         if (i.toLowerCase() == i) return i.toUpperCase();
         else return i.toLowerCase();
     })
     
-    return res.join('')
+    return my_string.join('')
     
 }
