801fd91e427123f320ae2ca78973470be12b6ea1
SuBin Hwang <103632489+subin114@users.noreply.github.com>
2024-04-03 19:36:51 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.js
@@ -1,7 +1,3 @@
 function solution(n, k) {
-    if (n >= 10) {
-        return (n * 12000) + (k * 2000) - (Math.floor(n / 10) * 2000)
-    } else {
-        return (n * 12000) + (k * 2000)
-    }
+     return n * 12000 + k * 2000 - parseInt(n / 10) * 2000;
 }
