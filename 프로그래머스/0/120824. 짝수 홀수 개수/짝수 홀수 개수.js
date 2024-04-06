4660b391f07d5f58442052df8183bf9f34a02c00
SuBin Hwang <103632489+subin114@users.noreply.github.com>
2024-04-07 03:25:55 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,11 +1,1 @@
-function solution(num_list) {
-    let a = 0,
-        b = 0;
-    
-    for (let i of num_list) {
-        if (i % 2 === 0) a++;
-        else b++;
-    }
-    
-    return [a, b]
-}
+const solution = nums => [nums.filter(n => n % 2 === 0).length, nums.filter(n => n % 2 !== 0).length]
