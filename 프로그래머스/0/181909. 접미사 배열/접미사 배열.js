06d142e722f0e16b87e2a7702b1db1fe8242882c
SuBin Hwang <103632489+subin114@users.noreply.github.com>
2024-04-16 12:54:44 +0900

    [level 0] Title: 접미사 배열, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.js
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.js
@@ -1,9 +1,1 @@
-function solution(my_string) {
-    let arr = [];
-    
-    for (let i = 0; i < my_string.length; i++) {
-        arr.push(my_string.substring(i))
-    }
-    
-    return arr.sort()
-}
+const solution = my_string => Array.from({ length: my_string.length }, (_, i) => my_string.substring(i)).sort()
