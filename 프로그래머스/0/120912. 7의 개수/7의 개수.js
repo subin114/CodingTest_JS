1f19f0e05c4669109359c1fcbe980fd5d03aa118
SuBin Hwang <103632489+subin114@users.noreply.github.com>
2024-04-18 16:38:30 +0900

    [level 0] Title: 7의 개수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120912. 7의 개수/7의 개수.js
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.js
@@ -1,9 +1,1 @@
-function solution(array) {
-    let count = 0;
-    
-    array.join('').split('').forEach(n => {
-        if (n === "7") count += 1
-    })
-    
-    return count
-}
+const solution = array => array.join('').split('').filter(n => String(n).includes('7')).length
