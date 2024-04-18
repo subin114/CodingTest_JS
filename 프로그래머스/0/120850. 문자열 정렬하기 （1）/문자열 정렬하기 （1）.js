284989bc543e9d0de512aeb67a53fd221e7aec90
SuBin Hwang <103632489+subin114@users.noreply.github.com>
2024-04-18 19:13:34 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -1,7 +1,2 @@
-function solution(my_string) {
-    let arr = my_string.split('').filter((num) => {
-        if (!isNaN(num)) return num
-    })
-    
-    return arr.map((i) => +i).sort()
-}
+const solution = my_string => [...my_string].filter(n => !isNaN(n)).map(Number).sort()
+
