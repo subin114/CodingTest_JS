d7476a2dc2935d84f9efd740ca30b85a95f1bd44
SuBin Hwang <103632489+subin114@users.noreply.github.com>
2024-04-25 23:53:12 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -0,0 +1,6 @@
+const solution = numbers => {
+    const arr = [ "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" ]
+    arr.forEach((v, i) => numbers = numbers.replaceAll(v, i))
+    
+    return +numbers
+}
